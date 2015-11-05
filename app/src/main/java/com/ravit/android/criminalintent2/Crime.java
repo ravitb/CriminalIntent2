8b3964cd6e85411ce64af10817b057e581cd6795
ravitb <ravit@rounds.com>
2015-11-05 18:15:00 +0200

    Chapter 08

--- app/src/main/java/com/ravit/android/criminalintent2/Crime.java
+++ app/src/main/java/com/ravit/android/criminalintent2/Crime.java
@@ -1,5 +1,6 @@
 package com.ravit.android.criminalintent2;
 
+import java.util.Date;
 import java.util.UUID;
 
 /**
@@ -9,9 +10,28 @@ public class Crime {
 
 	private String mTitle;
 	private UUID mId;
+	private Date mDate;
+	private Boolean mSolved;
 
 	public Crime() {
 		mId = UUID.randomUUID();
+		mDate = new Date();
+	}
+
+	public Date getDate() {
+		return mDate;
+	}
+
+	public void setDate(Date date) {
+		mDate = date;
+	}
+
+	public Boolean getSolved() {
+		return mSolved;
+	}
+
+	public void setSolved(Boolean solved) {
+		mSolved = solved;
 	}
 
 	public UUID getId() {
